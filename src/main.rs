581167a91df21812e64488f4017175c398421f53
Björn Weinehall <gecko@acc.umu.se>
2019-10-21 19:55:15 +0200

    Prepare for dual-licensing

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,11 @@
+// Copyright 2019 Björn Weinehall
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 #![no_std]
 #![no_main]
-
-/*
- * Copyright Björn Weinehall 2019
- *
- * References:
- *
- * STM32F407 Reference Manual
- *   ST Microelectronics
- *   https://www.st.com/content/ccc/resource/technical/document/reference_manual/3d/6d/5a/66/b4/99/40/d4/DM00031020.pdf/files/DM00031020.pdf/jcr:content/translations/en.DM00031020.pdf
- *
- * STM32F407G-DISC1 User Manual
- *   ST Microelectronics
- *   https://www.st.com/content/ccc/resource/technical/document/user_manual/70/fe/4a/3f/e7/e1/4f/7d/DM00039084.pdf/files/DM00039084.pdf/jcr:content/translations/en.DM00039084.pdf
- *
- * How to construct a Vectrex Multicart (Very simple)
- *   Tursi (M. Brent)
- *   http://www.harmlesslion.com/text/vectrex_multicart.htm
- *
- * Emulating a GameBoy Cartridge with an STM32F4
- *   Dhole
- *   http://dhole.github.io/post/gameboy_cartridge_emu_1/
- *
- * Extreme Vectrex multicart
- *   Jeroen "Sprite" Domburg
- *   https://spritesmods.com/?art=veccart&page=1
- */
-
-/*
- * Which development is board used shouldn't matter much. However,
- * make sure the used GPIOs aren't connected to board components
- * such as LEDs, USB, IMU, etc...
- *
- * (The plentiful on-board stuff on the STM32F407G-DISC1 is the reason
- *  why the address lines are such a mess)
- *
- * Vectrex  STM32F407   Note
- * *OE      PA1         "inverse CPU E clock". Triggers cart emulation interrupt once every cycle.
- * *CE      PA2         "inverse Chip Enable". A15 on CPU? Only push data when low.
- * *WE      PA3         "inverse Write Enable". Not used (yet)
- *
- * A0       PD0
- * A1       PD1
- * A2       PD2
- * A3       PD3
- * A4       PE4
- * A5       PE5
- * A6       PD6
- * A7       PD7
- * A8       PD8
- * A9       PD9
- * A10      PD10
- * A11      PD11
- * A12      PB12
- * A13      PB13
- * A14      PB14
- * X        PB15       "Software controlled line" - Not used (yet)
- *
- * D0       PE8
- * ..       ..
- * D7       PE15
- *
- * GND      GND
- * +5V      +5V        Powers the board. Just make sure to unplug the USB cable!
- *
- * *HALT    -
- * *CART    -
- * *NMI     -
- * *IRQ     -
- */
 
 // cargo objdump --release -- -d -S --disassemble-functions=EXTI1 target/thumbv7em-none-eabihf/release/vectrex-cart
 
